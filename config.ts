8247090285bab7c7fc78dc858c0f5e709620bd1e
soonfy <soonfy@163.com>
2017-04-28 15:14:36 +0800

    ua

--- config.ts
+++ config.ts
@@ -28,7 +28,8 @@ const options = {
   method: 'GET',
   headers: {
     'Host': weixinhost,
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+    // 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36'
   },
   timeout: 1000 * 30
 };
