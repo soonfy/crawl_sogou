c9dec9ebbaa60785bf5404e8551d6fd1d198f138
soonfy <soonfy@163.com>
2017-05-03 10:06:51 +0800

    demo

--- demo.ts
+++ demo.ts
@@ -34,39 +34,37 @@ const start = async (weixin = 'rmrbwx', time = 60) => {
       tool.clog(++index);
 
       // content 采集
-      // let temp = `${item.content_url.slice(0, -1)}${config.suf}`,
-      //   sourceurl = item.source_url;
-      // temp = temp.replace(/amp;/g, '');
-
-      // if (temp.includes('mp.weixin.qq.com') && (!temp.includes('signature'))) {
-      //   tool.clog(temp);
-      //   tool.clog(`上面链接没有阅读量接口提取不到阅读量，跳过`);
-      //   continue;
-      // }
-      // let jsonurl = `${config.preContent}${temp}`;
-      // let _content = {
-      //   username: username,
-      //   contenturl: item.content_url,
-      //   sourceurl: item.source_url,
-      //   jsonurl: jsonurl,
-      //   title: item.title,
-      //   digest: item.digest,
-      //   cover: item.cover
-      // }
-      // await tool.sleep(time);
-      // let cdata = await getContent(_content);
-      // if (!cdata) {
-      //   tool.cerror(`文章内容数据返回 null`);
-      //   continue;
-      // }
-      // cdata = JSON.parse(cdata);
-      // tool.clog(cdata.title);
-
-      // if (!cdata.title) {
-      //   tool.cerror(temp);
-      //   tool.cerror(`文章涉嫌违规已被删除。`);
-      //   continue;
-      // }
+      let temp = `${item.content_url.slice(0, -1)}${config.suf}`,
+        sourceurl = item.source_url;
+      temp = temp.replace(/amp;/g, '');
+      if (temp.includes('mp.weixin.qq.com') && (!temp.includes('signature'))) {
+        tool.clog(temp);
+        tool.clog(`上面链接没有阅读量接口提取不到阅读量，跳过`);
+        continue;
+      }
+      let jsonurl = `${config.preContent}${temp}`;
+      let _content = {
+        username: username,
+        contenturl: item.content_url,
+        sourceurl: item.source_url,
+        jsonurl: jsonurl,
+        title: item.title,
+        digest: item.digest,
+        cover: item.cover
+      }
+      await tool.sleep(time);
+      let cdata = await getContent(_content);
+      if (!cdata) {
+        tool.cerror(`文章内容数据返回 null`);
+        continue;
+      }
+      cdata = JSON.parse(cdata);
+      tool.clog(cdata.title);
+      if (!cdata.title) {
+        tool.cerror(temp);
+        tool.cerror(`文章涉嫌违规已被删除。`);
+        continue;
+      }
 
       // read 采集
       // let commenturl = `${config.preRead}${temp.slice(2)}`;
