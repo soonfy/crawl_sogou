800aad5f07253271c2f7f1751ee206299501a686
soonfy <soonfy@163.com>
2017-12-22 10:16:50 +0800

    chrome

--- test.ts
+++ test.ts
@@ -8,21 +8,17 @@ const iPhone = devices['iPhone 6'];
 const start = async () => {
   try {
     console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
-    let index = 0;
-    let ids = fs.readFileSync('./weixin.txt', 'utf-8').split('\n');
-    // console.log(ids);
 
     const browser = await puppeteer.launch({
       headless: true
     });
     const page = await browser.newPage();
-    await page.goto('http://weixin.sogou.com/');
+    await page.goto('http://www.baidu.com/');
     console.log(await page.title());
     await browser.close();
 
   } catch (error) {
     console.error(error);
-    await start();
   }
 }
 
